0b85eed42872209ef602d830e86dd56cbacd209d
Pavel Merekin <merekin33@gmail.com>
2025-02-12 15:02:47 +0700

    todo app version 2 is ready

---
+++ 25-todo-v2/src/components/Todos/TodoForm.js
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import Button from '../UI/Button';
+import styles from './TodoForm.module.css';
+
+function TodoForm({ addTodo }) {
+  const [text, setText] = useState('');
+
+  const onSubmitHandler = event => {
+    event.preventDefault();
+    addTodo(text);
+    setText('');
+  };
+
+  return (
+    <div className={styles.todoFormContainer}>
+      <form onSubmit={onSubmitHandler}>
+        <input
+          placeholder="Enter new todo"
+          autoComplete="off"
+          value={text}
+          onChange={event => setText(event.target.value)}
+        />
+        <Button type="submit" title="Submit">
+          Submit
+        </Button>
+      </form>
+    </div>
+  );
+}
+
+export default TodoForm;
