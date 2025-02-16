c9cecabbbc5e91d57b7c38e75be59f000326a07f
Pavel Merekin <merekin33@gmail.com>
2025-02-16 19:07:44 +0700

    BookForm, BookList, Filter components added

--- 30-react-redux/frontend/src/App.js
+++ 30-react-redux/frontend/src/App.js
@@ -1,9 +1,23 @@
+// import Filter from './components/Filter/Filter'
+import BookForm from './components/BookForm/BookForm'
+import BookList from './components/BooksList/BookList'
 import './App.css'
 
 function App() {
   return (
-    <div className='app'>
-      <h1>Book Library App</h1>
+    <div className="app">
+      <header className="app-header">
+        <h1>Book Library App</h1>
+      </header>
+      <main className="app-main">
+        <div className="app-left-column">
+          <BookForm />
+        </div>
+        <div className="app-right-column">
+          {/* <Filter /> */}
+          <BookList />
+        </div>
+      </main>
     </div>
   )
 }
