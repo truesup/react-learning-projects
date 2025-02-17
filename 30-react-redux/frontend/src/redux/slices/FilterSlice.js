47b2513c8d0a5a85b217ac09dd0af173d1fe00a3
Pavel Merekin <merekin33@gmail.com>
2025-02-17 20:38:14 +0700

    only favorite filter

--- 30-react-redux/frontend/src/redux/slices/FilterSlice.js
+++ 30-react-redux/frontend/src/redux/slices/FilterSlice.js
@@ -3,6 +3,7 @@ import { createSlice } from '@reduxjs/toolkit'
 const initialState = {
   title: '',
   author: '',
+  onlyFavorite: false,
 }
 
 const filterSlice = createSlice({
@@ -19,12 +20,20 @@ const filterSlice = createSlice({
     setAuthorFilter: (state, action) => {
       state.author = action.payload
     },
-    resetFilters: state => initialState,
+    setOnlyFavoriteFilter: state => {
+      state.onlyFavorite = !state.onlyFavorite
+    },
+    resetFilters: () => initialState,
   },
 })
 
-export const { setTitleFilter, setAuthorFilter, resetFilters } =
-  filterSlice.actions
+export const {
+  setTitleFilter,
+  setAuthorFilter,
+  setOnlyFavoriteFilter,
+  resetFilters,
+} = filterSlice.actions
 export const selectTitleFilter = state => state.filter.title
 export const selectAuthorFilter = state => state.filter.author
+export const selectOnlyFavoriteFilter = state => state.filter.onlyFavorite
 export default filterSlice.reducer
